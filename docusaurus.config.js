b51ae3ef3e9f826001bb51b525fa30df70422529
mrpavan <mr.pavan@gmail.com>
2022-12-29 16:42:17 +0800

    added github ations workflow

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tech-talks-in', // Usually your GitHub org/user name.
   projectName: 'tech-talks-in.github.io', // Usually your repo name.
-  deploymentBranch: 'gh_pages',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
