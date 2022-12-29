a6cda55b7e45e30f41b75507e3c06d0314ae45c9
mrpavan <mr.pavan@gmail.com>
2022-12-29 15:00:34 +0800

    updated site url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'tech-talks',
   tagline: 'Tech news, tips, and insights for the modern world',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://tech-talks-in.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -18,6 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tech-talks-in', // Usually your GitHub org/user name.
   projectName: 'tech-talks-in', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
